949e169d5ee1db382dc5def88070eead17588e21
lennijusten <ljusten@wisc.edu>
2022-06-24 12:45:44 -0400

    Create UK-data processing script

---
+++ UK_airport_data.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import itertools
+import numpy as np
+
+# Inputs
+airport = 'Bradley'
+avg_speed = 805  # Rough average commercial passenger jet flight speed (k/h)
+
+# Init data
+df4 = pd.read_csv('UK data/Table 05 Air Transport Movements.csv')  # total aircraft movements (domestic/international)
+df5 = pd.read_csv('UK data/Table 08 Air Pax by Type and Nat of Op.csv')  # total passengers (domestic/foreign-EU/foreign-nonEU)
+
+
+
+
+
+
+
+
+
